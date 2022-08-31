5527358ba6e0a7018debdd059d9ca1cb4664e821
TgT60 <dima.icigo@yandex.ru>
2022-08-31 16:18:25 +0300

    Calculator
    Change solution

--- Program.cs
+++ Program.cs
@@ -7,41 +7,63 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Select an operation");
+            Console.WriteLine(" Select an operation\n 1)+ \n 2)- \n 3)% \n 4)* \n 5)/ \n 6)SR");
             string operation = Console.ReadLine();
-            Console.WriteLine("Enter the first number") ;
-            string number1 = Console.ReadLine();
-            Console.WriteLine("Enter the second number"); 
-            string number2 = Console.ReadLine();
-            int a = Convert.ToInt32(number1);
-            int b = Convert.ToInt32(number2);
             switch (operation)
             {
                 case "+":
-                    Console.WriteLine(a+b);
+                    Console.WriteLine("Enter the first number");
+                    string number1 = Console.ReadLine();
+                    Console.WriteLine("Enter the second number");
+                    string number2 = Console.ReadLine();
+                    int plus1 = Convert.ToInt32(number1);
+                    int plus2 = Convert.ToInt32(number2);
+                    Console.WriteLine("Result is addition = " + (plus1+plus2));
                     Console.ReadLine();
                     break;
                 case "-":
-                    Console.WriteLine(a-b);
+                    Console.WriteLine("Enter the first number");
+                    string number3 = Console.ReadLine();
+                    Console.WriteLine("Enter the second number");
+                    string number4 = Console.ReadLine();
+                    int minus1 = Convert.ToInt32(number3);
+                    int minus2 = Convert.ToInt32(number4);
+                    Console.WriteLine("Result of subtraction = "+(minus1-minus2));
                     Console.ReadLine();
                     break;
                 case "%":
-                    double g = Convert.ToDouble(a);
-                    Console.WriteLine(g/100);
+                    Console.WriteLine("Enter number");
+                    string number5 = Console.ReadLine();
+                    double percent = Convert.ToDouble(number5);
+                    Console.WriteLine("One percent = "+(percent / 100));
                     Console.ReadLine();
                     break;
                 case "*":
-                    Console.WriteLine(a*b);
+                    Console.WriteLine("Enter the first number");
+                    string number6 = Console.ReadLine();
+                    int multiplication1 = Convert.ToInt32(number6);
+                    Console.WriteLine("Enter the second number");
+                    string number7 = Console.ReadLine();
+                    int multiplication2 = Convert.ToInt32(number7);
+                    Console.WriteLine("Result of multiplication = " + (multiplication1 * multiplication2));
                     Console.ReadLine();
                     break;
                 case "/":
-                    double c = Convert.ToDouble(a);
-                    double d = Convert.ToDouble(b);
-                    Console.WriteLine(c%d);
+                    Console.WriteLine("Enter the first number");
+                    string number8 = Console.ReadLine();
+                    Console.WriteLine("Enter the second number");
+                    string number9 = Console.ReadLine();
+                    double demultiplication1 = Convert.ToDouble(number8);
+                    double demultiplication2 = Convert.ToDouble(number9);
+                    Console.WriteLine("Result of demultiplication = " + (demultiplication1 /demultiplication2));
+                    Console.ReadKey();
                     break;
                 case"SR":
-                    double square_root = Math.Sqrt(a);
-                    Console.WriteLine(square_root);
+                    Console.WriteLine("Enter number");
+                    string number10 = Console.ReadLine();   
+                    double sr = Convert.ToDouble(number10);
+                    double square_root = Math.Sqrt(sr);
+                    Console.WriteLine("The square root = " + square_root);
                     Console.ReadLine();
                     break;
 
