a8cc2b4397ce86ad070fae0e609f67c3b9876ff3
TgT60 <dima.icigo@yandex.ru>
2022-09-01 20:48:43 +0300

    change met

--- Program.cs
+++ Program.cs
@@ -32,6 +32,13 @@ namespace ConsoleApp2
                     break;
             }
         }
+        static double SecondNum()
+        {
+            Console.WriteLine("Enter the second number");
+            string number2 = Console.ReadLine();
+            double plus2 = Convert.ToInt32(number2);
+            return plus2;
+        }
         static double FirstNum()
         {
             Console.WriteLine("Enter the first number");
@@ -42,18 +49,14 @@ namespace ConsoleApp2
         static void Plus()
         {
             double plus1 = FirstNum();
-            Console.WriteLine("Enter the second number");
-            string number2 = Console.ReadLine();
-            int plus2 = Convert.ToInt32(number2);
+            double plus2 = SecondNum();
             Console.WriteLine("Result is addition = " + (plus1 + plus2));
             Console.ReadLine();
         }
         static void Minus()
         {
             double minus1 = FirstNum();
-            Console.WriteLine("Enter the second number");
-            string number4 = Console.ReadLine();
-            int minus2 = Convert.ToInt32(number4);
+            double minus2 = SecondNum();
             Console.WriteLine("Result of subtraction = " + (minus1 - minus2));
             Console.ReadLine();
         }
@@ -68,18 +71,14 @@ namespace ConsoleApp2
         static void Mult()
         {
             double multiplication1 = FirstNum();
-            Console.WriteLine("Enter the second number");
-            string number7 = Console.ReadLine();
-            int multiplication2 = Convert.ToInt32(number7);
+            double multiplication2 = SecondNum();
             Console.WriteLine("Result of multiplication = " + (multiplication1 * multiplication2));
             Console.ReadLine();
         }
         static void DeMult()
         {
             double demultiplication1 = FirstNum();
-            Console.WriteLine("Enter the second number");
-            string number9 = Console.ReadLine();
-            double demultiplication2 = Convert.ToDouble(number9);
+            double demultiplication2 = SecondNum();
             Console.WriteLine("Result of demultiplication = " + (demultiplication1 / demultiplication2));
             Console.ReadKey();
         }
