2d943bcf73308d146ec8e01d64d00044d05f7094
TgT60 <dima.icigo@yandex.ru>
2022-09-06 01:46:49 +0300

    Paragraph 2
    -Ex1
    -completed

---
+++ Hw3.2/Ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1N");
+            string one = Console.ReadLine();
+            double NumO = Convert.ToDouble(one);
+            Console.WriteLine("2N");
+            string two = Console.ReadLine();
+            double NumT = Convert.ToDouble(two);
+            if (NumO == NumT)
+            {
+                double c = (NumO + NumT) * 3;
+                Console.WriteLine(c);
+                Console.ReadKey();
+            }
+            else
+            {
+                double b = NumT + NumO;
+                Console.WriteLine(b);
+                Console.ReadKey();
+            }
+        
+
+        }
+    }
+}
