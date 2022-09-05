a40782b090d7f681a0ec04caec1cdb34fd92a5c5
TgT60 <dima.icigo@yandex.ru>
2022-09-06 02:19:39 +0300

    Paragraph 2
    -Ex4
    -Complete

---
+++ Hw3.2/Ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1N");
+            string one = Console.ReadLine();
+            double NumO = Convert.ToDouble(one);
+            if (NumO >= 90 && NumO <= 110  )
+            {
+                Console.WriteLine(true);
+                Console.ReadKey();      
+            }
+            else
+            {
+                Console.WriteLine(false);
+                Console.ReadKey();
+            }
+        }
+    }
+}
