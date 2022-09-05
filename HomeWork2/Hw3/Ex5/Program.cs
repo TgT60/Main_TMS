17b488aa6872f8d43efc2e1b763f58f396b875bb
TgT60 <dima.icigo@yandex.ru>
2022-09-05 22:29:27 +0300

    Ex5
    -completed

---
+++ HomeWork2/Hw3/Ex5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine(a);
+            int b = 6;
+            Console.WriteLine(b);
+            int tmp;
+            tmp = a;
+            a = b;
+            b = tmp;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadKey();
+
+        }
+    }
+}
