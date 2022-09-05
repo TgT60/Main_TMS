760377ceb5c327666efece6fb0e0ec88399c17df
TgT60 <dima.icigo@yandex.ru>
2022-09-06 01:58:19 +0300

    Paragraph 2
    -Ex2
    -Completed

---
+++ Hw3.2/Ex2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1N");
+            string one = Console.ReadLine();
+            double NumO = Convert.ToDouble(one);
+            if (NumO == 51)
+            {
+                Console.WriteLine(NumO = 0);
+                Console.ReadKey();
+            }
+            else if(NumO<51)
+            {
+                Console.WriteLine(51-NumO);
+                Console.ReadKey();
+            }
+            else if(NumO>51)
+            {
+                Console.WriteLine(NumO*3%51);
+                Console.ReadKey( );
+
+            }
+
+        }
+    }
+}
