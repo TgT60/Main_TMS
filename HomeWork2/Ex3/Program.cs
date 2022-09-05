c4416619b419b15482513854de48183c20ad5c5f
TgT60 <dima.icigo@yandex.ru>
2022-09-05 21:54:04 +0300

    Ex3
    -completed

---
+++ HomeWork2/Ex3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            string num_one = Console.ReadLine();
+            double  a = Convert.ToDouble(num_one);
+            Console.WriteLine("Enter second number");
+            string num_two = Console.ReadLine();
+            double b = Convert.ToDouble(num_two);
+            Console.WriteLine("Demult " + (a / b));
+            Console.ReadKey();
+        }
+    }
+}
