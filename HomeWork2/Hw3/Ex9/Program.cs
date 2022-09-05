7ca88473ecb9593577527aa48f7eb3da67937261
TgT60 <dima.icigo@yandex.ru>
2022-09-05 23:35:43 +0300

    Ex9
    -completed

---
+++ HomeWork2/Hw3/Ex9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1N");
+            string one = Console.ReadLine();
+            double NumO = Convert.ToDouble(one);
+            Console.WriteLine("2N");
+            string two = Console.ReadLine();
+            double NumT = Convert.ToDouble(two);
+            Console.WriteLine("3N");
+            string three = Console.ReadLine();
+            double NumThree = Convert.ToDouble(three);
+            Console.WriteLine("4N");
+            string four = Console.ReadLine();
+            double NumF = Convert.ToDouble(four);
+            Console.WriteLine((NumO+NumT+NumThree+NumF)/4);
+            Console.ReadKey();
+        }
+    }
+}
