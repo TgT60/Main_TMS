b282d646cf339b11b41011a78e49924ddec24fa6
TgT60 <dima.icigo@yandex.ru>
2022-09-05 23:25:54 +0300

    Ex8
    -completed

---
+++ HomeWork2/Hw3/Ex8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double b = 0;
+            for (int i = 5; b <= 10; b++)
+            {
+                Console.WriteLine(i*b);
+                
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
