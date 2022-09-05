82dfa231d49c69e80213602dd9cba15760b5081b
TgT60 <dima.icigo@yandex.ru>
2022-09-06 02:08:14 +0300

    Paragraph
    -Ex3
    -Complete

---
+++ Hw3.2/Ex3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1N");
+            string one = Console.ReadLine();
+            double NumO = Convert.ToDouble(one);
+            Console.WriteLine("2N");
+            string two = Console.ReadLine();
+            double NumT = Convert.ToDouble(two);
+            if (NumT == 30 || NumO == 30 || (NumO + NumT) == 30)
+            {
+                Console.WriteLine(true);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine(false);
+                Console.ReadKey();
+            }
+        }
+    }
+}
