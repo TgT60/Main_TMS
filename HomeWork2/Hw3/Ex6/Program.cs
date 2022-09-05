f82493efe286c8005e2f849319d30f89b62f21f0
TgT60 <dima.icigo@yandex.ru>
2022-09-05 22:45:40 +0300

    Ex6
    -completed

---
+++ HomeWork2/Hw3/Ex6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = Console.ReadLine();
+            int NumOne = Convert.ToInt32(one);
+            string two = Console.ReadLine();
+            int NumTwo = Convert.ToInt32(two);
+            string three = Console.ReadLine();
+            int NumThree = Convert.ToInt32(three);
+            Console.WriteLine(NumOne * NumTwo * NumThree);
+            Console.ReadKey();
+
+            
+        }
+    }
+}
