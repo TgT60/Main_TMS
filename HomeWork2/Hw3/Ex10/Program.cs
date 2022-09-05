521f2ffca3a1c00967cd821ff7eaba5195597e5b
TgT60 <dima.icigo@yandex.ru>
2022-09-05 23:48:04 +0300

    Ex10
    -completed

---
+++ HomeWork2/Hw3/Ex10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1N");
+            string one = Console.ReadLine();
+            double NumO = Convert.ToDouble(one);
+            Console.WriteLine("2N");
+            string two = Console.ReadLine();
+            double NumT = Convert.ToDouble(two);
+            Console.WriteLine("3N");
+            string three = Console.ReadLine();
+            double NumThree = Convert.ToDouble(three);
+            Console.WriteLine((NumO+NumT)*NumThree+" and "+(NumO*NumT+NumT*NumThree));
+            Console.ReadKey();
+        }
+    }
+}
