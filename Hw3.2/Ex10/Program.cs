3ffd03f83517411182735180c03f6af8ec52ea2e
TgT60 <dima.icigo@yandex.ru>
2022-09-06 02:31:55 +0300

    Paragraph 2
    -Ex10
    -Completed

---
+++ Hw3.2/Ex10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1N");
+            string one = Console.ReadLine();
+            double NumO = Convert.ToDouble(one);
+            if (NumO%3==0 || NumO%7==0)
+            {
+                Console.WriteLine(true);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine(false);
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
