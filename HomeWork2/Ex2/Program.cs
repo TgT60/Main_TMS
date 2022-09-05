4c38a64635362d55159b82dfdcff770ffba5c065
TgT60 <dima.icigo@yandex.ru>
2022-09-05 21:50:02 +0300

    Ex1
    -completed

---
+++ HomeWork2/Ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            string num_one = Console.ReadLine();
+            int a = Convert.ToInt32(num_one);
+            Console.WriteLine("Enter second number");
+            string num_two = Console.ReadLine();
+            int b = Convert.ToInt32(num_two);
+            Console.WriteLine("Sum ="+(a + b)) ;
+            Console.ReadKey();
+        }
+    }
+}
