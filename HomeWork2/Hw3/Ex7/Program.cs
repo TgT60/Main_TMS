ab1567b332933edf5064689916e96d16d1b7e764
TgT60 <dima.icigo@yandex.ru>
2022-09-05 23:15:25 +0300

    Ex7
    -completed

---
+++ HomeWork2/Hw3/Ex7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1N");
+            string one = Console.ReadLine();
+            double NumO = Convert.ToDouble(one);
+            Console.WriteLine("2N");
+            string two = Console.ReadLine();
+            double NumT = Convert.ToDouble(two);
+            Console.WriteLine((NumO+NumT)+"\n"+(NumO - NumT)+ "\n" +(NumO*NumT)+"\n"+(NumO/NumT)+"\n"+(NumO%NumT));
+            Console.ReadLine();
+        }
+    }
+}
