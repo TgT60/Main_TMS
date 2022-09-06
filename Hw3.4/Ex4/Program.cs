c2789d6f8c97a8f1e6620cdb5076fa3be608641e
TgT60 <dima.icigo@yandex.ru>
2022-09-06 13:23:20 +0300

    Paragraph 3
    -Ex4
    -Completed

---
+++ Hw3.4/Ex4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1N");
+            string NumO = Console.ReadLine();
+            int one = Convert.ToInt32(NumO);
+            if (one%400==0)
+            {
+                Console.WriteLine(one+ " leap year");
+                Console.ReadKey();
+            }
+            else if (one%100==0)
+            {
+                Console.WriteLine(one + " no leap year");
+                Console.ReadLine();
+            }
+            else if(one%4==0)
+            {
+                Console.WriteLine(one + " leap year");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine(one + " no leap year");
+                Console.ReadLine();
+            }
+        }
+    }
+}
