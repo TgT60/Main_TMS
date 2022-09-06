9a1b3b0901abc5c38fbae4aca79e4dd0a19b70fc
TgT60 <dima.icigo@yandex.ru>
2022-09-06 12:44:03 +0300

    Paragraph 3
    -Ex2
    -Completed

---
+++ Hw3.4/Ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1N");
+            string NumO = Console.ReadLine();
+            int one = Convert.ToInt32(NumO);
+            if (one%2==0)
+            {
+                Console.WriteLine(one +" = even");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine(one+ " = odd");
+                Console.ReadKey();
+            }
+        }
+    }
+}
