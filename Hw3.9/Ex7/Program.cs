1a51ef69bd2aa78057c8f57e4d97490969b217b5
TgT60 <dima.icigo@yandex.ru>
2022-09-06 17:29:22 +0300

    Paragraph 9
    -Ex7
    -Completed

---
+++ Hw3.9/Ex7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter anouther number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Step(a,b);
+            Console.ReadKey();
+        }
+        static void Step(int a,int b)
+        {
+            double c = Math.Pow(a, b);
+            Console.WriteLine(c);
+            
+
+
+        }
+
+    }  
+}
