a57a5b3c80e1d04b7992309ab79d0261b50ecb14
TgT60 <dima.icigo@yandex.ru>
2022-09-06 03:01:20 +0300

    Paragraph 2
    -Ex13
    -Completed

---
+++ Hw3.2/Ex13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1N");
+            string one = Console.ReadLine();
+            double NumO = Convert.ToDouble(one);
+            Console.WriteLine("2N");
+            string two = Console.ReadLine();
+            double NumT = Convert.ToDouble(two);
+            if ((NumO < 0 || NumO > 100) && (NumT< 0 || NumT > 100))
+            {
+                Console.WriteLine(true);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine(false);
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
