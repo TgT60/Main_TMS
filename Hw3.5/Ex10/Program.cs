7c26970c11447c9ec0dcbd76c31ad9b48743ac75
TgT60 <dima.icigo@yandex.ru>
2022-09-06 16:15:56 +0300

    Paragraph 5
    -Ex10
    -Completed

---
+++ Hw3.5/Ex10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            string m = Console.ReadLine();
+            int n = Convert.ToInt32(m);
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(1+j);
+                }
+                Console.WriteLine();
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
