8c9d49fc95f0b315c6e28330067650d61d6772ea
TgT60 <dima.icigo@yandex.ru>
2022-09-06 03:12:27 +0300

    Paragraph 2
    -Ex14
    -Completed

---
+++ Hw3.2/Ex14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1N");
+            string one = Console.ReadLine();
+            int NumO = Convert.ToInt32(one);
+            Console.WriteLine("2N");
+            string two = Console.ReadLine();
+            int NumT = Convert.ToInt32(two);
+            if (NumO >= 100 || NumT >= 100)
+            {
+                Console.WriteLine(true);
+                Console.ReadKey();
+            }
+            else if (NumO<200 || NumT<200)
+            {
+                Console.WriteLine(false);
+                Console.ReadKey();
+            }
+        }
+    }
+}
