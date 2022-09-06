d6fd6b9ae3aed7f263bd1fa97afbc98a63a053a5
TgT60 <dima.icigo@yandex.ru>
2022-09-06 16:30:23 +0300

    Paragraph 9
    -Ex1
    -Completed

---
+++ Hw3.9/Ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wellocme();
+            Nice();
+            Console.ReadKey();
+        }
+        static void Wellocme ()
+        {
+            Console.WriteLine("Welcome friends ") ;
+
+        }
+        static void Nice()
+        {
+            Console.WriteLine("Have a nice day");
+        }
+    }
+}
