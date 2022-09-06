66bc41b5bdd30ad1424e095562e720204ac8b9df
TgT60 <dima.icigo@yandex.ru>
2022-09-06 13:04:26 +0300

    Paragraph 3
    -Ex3
    -Completed

---
+++ Hw3.4/Ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1N");
+            string NumO = Console.ReadLine();
+            int one = Convert.ToInt32(NumO);
+            if (one>0)
+            {
+                Console.WriteLine(one+" positive number");
+                Console.ReadKey();
+            }
+            else if (one<0)
+            {
+                Console.WriteLine(one+" negative number");
+                Console.ReadKey();
+            }
+        }
+    }
+}
