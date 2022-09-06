3c8705a807d88d0bbe75d52f50ffd28d2c8741d5
TgT60 <dima.icigo@yandex.ru>
2022-09-06 15:42:42 +0300

    Paragraph 5
    -Ex7
    -Completed

---
+++ Hw3.5/Ex7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            string m = Console.ReadLine();
+            int n = Convert.ToInt32(m);
+            for (int i = 1; i <=10; i++)
+            {
+                for (int j = 1; j <=n; j++)
+                {
+                    if (j<=n-1)
+                    {
+                        Console.Write(i + " * " + j + " = " + ( i* j));
+                        Console.ReadKey();
+                    }
+                    else
+                        Console.Write(i + " * " + j + " = " + (i * j));
+
+
+                }
+                Console.WriteLine("\n");
+            }
+
+
+       
+
+            
+        }
+    }
+}
