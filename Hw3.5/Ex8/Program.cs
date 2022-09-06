0fec78a92dc3a8c15a1d5ab5452e3b78a6a448f1
TgT60 <dima.icigo@yandex.ru>
2022-09-06 15:59:46 +0300

    Paragraph 5
    -Ex8
    -Completed

---
+++ Hw3.5/Ex8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            string m = Console.ReadLine();
+            int n = Convert.ToInt32(m);
+            int sum = 0;
+            for (int i = 1; i <=n ; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    n++;
+                }
+                else
+                {
+
+                    Console.WriteLine(i);
+                    Console.ReadKey();
+                    sum = sum + i;
+                    
+                }
+                
+               
+           
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+
+    }
+}
