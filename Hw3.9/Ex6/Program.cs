fd1fda109cb1c609911a232ff0275ec9242d5845
TgT60 <dima.icigo@yandex.ru>
2022-09-06 17:16:52 +0300

    Paragraph 9
    -Ex6
    -Completed

---
+++ Hw3.9/Ex6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter anouther number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Solut(a, b);
+            Console.ReadKey();
+        }
+        static void Solut(int a, int b)
+        {
+            int t;
+            t = a;
+            a = b;
+            b = t;
+            Console.WriteLine("Now the 1st number is " + a + " and the 2nd number is : " + b);
+        }
+    }
+}
