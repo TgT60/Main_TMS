bd8e0b7aa20b8fc55b69d872ebfb0580589f877e
TgT60 <dima.icigo@yandex.ru>
2022-09-06 03:46:47 +0300

    Paragraph 2
    -Ex16
    -Completed

---
+++ Hw3.2/Ex16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1N");
+            string one = Console.ReadLine();
+            double NumO = Convert.ToDouble(one);
+            Console.WriteLine("2N");
+            string two = Console.ReadLine();
+            double NumT = Convert.ToDouble(two);
+            if ((NumO >= 20 && NumO <= 50) && (NumT >= 20 && NumT <= 50))
+            {
+                Console.WriteLine(false);
+                Console.ReadKey();
+            }
+            else
+            {
+
+                Console.WriteLine(true);
+                Console.ReadKey();
+            }    
+        }
+    }
+}
