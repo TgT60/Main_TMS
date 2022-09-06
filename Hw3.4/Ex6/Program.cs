8e95f0b6c9cf6e6c3ee49db3aa77cb635c6d9291
TgT60 <dima.icigo@yandex.ru>
2022-09-06 13:35:54 +0300

    Paragraph 3
    -Ex6
    -Completed

---
+++ Hw3.4/Ex6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)    
+        {
+            Console.WriteLine("1N");
+            string NumO = Console.ReadLine();
+            int one = Convert.ToInt32(NumO);
+            int n;
+            if (one >0)
+            {
+                Console.WriteLine(n=1);
+                Console.ReadKey();
+            }
+            else if (one<0)
+            {
+                Console.WriteLine(n=-1);
+                Console.ReadKey();
+            }
+            else if (one==0)
+            {
+                Console.WriteLine(n=0);
+                Console.ReadKey();
+            }
+        }
+    }
+}
