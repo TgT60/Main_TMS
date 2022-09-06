91e7ed0fb9548074f803f42dc18bc9c56c22922d
TgT60 <dima.icigo@yandex.ru>
2022-09-06 15:15:18 +0300

    Paragraph 5
    -Ex5
    -Completed

---
+++ Hw3.5/Ex5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            string m = Console.ReadLine();
+            int n = Convert.ToInt32(m);
+            for (int i = 0; i <=n; i++)
+            {
+                Console.WriteLine("Number is : "+ i + " and cube of the " + i + " is " + (i*i*i));
+                Console.ReadKey();
+            }
+        }
+    }
+}
