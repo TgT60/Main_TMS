7becbdefa525f1da230b989284b880a8b858b1f0
TgT60 <dima.icigo@yandex.ru>
2022-09-06 13:41:55 +0300

    Paragraph 3
    -Ex7
    -Completed

---
+++ Hw3.4/Ex7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your heigth");
+            string NumO = Console.ReadLine();
+            int one = Convert.ToInt32(NumO);
+            if (one >=180)
+            {
+                Console.WriteLine("You tall");
+                Console.ReadKey();
+            }
+            else if (one <=180 && one >=160)
+            {
+                Console.WriteLine("You normal heigth" );
+                Console.ReadKey();
+            }
+            else if (one<160)
+            {
+                Console.WriteLine("You short");
+                Console.ReadKey();
+            }
+        }
+    }
+}
