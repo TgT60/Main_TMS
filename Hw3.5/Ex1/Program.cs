7ab09ff774f7d71cc4a71ffd4465a7e276599074
TgT60 <dima.icigo@yandex.ru>
2022-09-06 14:28:22 +0300

    Paragraph 5
    -Ex1
    -Completed

---
+++ Hw3.5/Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i<=10; i++)
+            {
+                Console.Write(i);
+                Console.ReadKey();
+            }
+          
+        }
+    }
+}
