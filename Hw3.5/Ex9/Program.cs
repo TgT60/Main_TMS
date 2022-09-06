e0c0520a4fa748ccc17ba8a7395996d3ffcbd34b
TgT60 <dima.icigo@yandex.ru>
2022-09-06 16:13:13 +0300

    Paragraph 5
    -Ex9
    -Completed

---
+++ Hw3.5/Ex9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            string m = Console.ReadLine();
+            int n = Convert.ToInt32(m);
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*"); 
+                }
+                Console.WriteLine();
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
