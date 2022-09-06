3138e49f7712495361643e4e4867f8f70060fb12
TgT60 <dima.icigo@yandex.ru>
2022-09-06 14:53:54 +0300

    Paragraph 5
    -Ex3
    -Completed

---
+++ Hw3.5/Ex3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Data");
+            string m = Console.ReadLine();
+            int n = Convert.ToInt32(m);
+            int sum = 0 ;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(i);
+                Console.ReadKey();
+                sum = sum + i;
+                if(i == n)
+                {
+                    Console.WriteLine(sum);
+                    Console.ReadKey();
+                }
+            }
+
+        }
+    }
+}
