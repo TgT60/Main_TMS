13bf6b9a2c788110535d775f787e17e9758976c1
TgT60 <dima.icigo@yandex.ru>
2022-09-06 15:21:09 +0300

    Paragraph 5
    -Ex6
    -Completed

---
+++ Hw3.5/Ex6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            string m = Console.ReadLine();
+            int n = Convert.ToInt32(m);
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(n+ " X " + i +" = "+ (i*n) );
+                Console.ReadLine();
+            }
+        }
+    }
+}
