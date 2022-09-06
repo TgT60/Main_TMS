276fb5e8db3427ed731907bc1f4d6881dbad7e39
TgT60 <dima.icigo@yandex.ru>
2022-09-06 13:58:16 +0300

    Paragraph 3
    -Ex9
    -Completed

---
+++ Hw3.4/Ex9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the value for X coordinate ");
+            string NumO = Console.ReadLine();
+            int one = Convert.ToInt32(NumO);
+            Console.WriteLine("Input the value for Y coordinate ");
+            string NumT = Console.ReadLine();
+            int two = Convert.ToInt32(NumT);
+            if (one > 0 && two > 0)
+            {
+                Console.WriteLine("The coordinate point" + "(" + (one) + "," + (two) + ")" + "lies in the First quadrant.");
+                Console.ReadKey();
+            }
+            else if (one < 0 && two > 0)
+            {
+                Console.WriteLine("The coordinate point" + "(" + (one) + "," + (two) + ")" + "lies in the Second quadrant.");
+                Console.ReadKey();
+            }
+            else if(one<0 && two<0)
+            {
+                Console.WriteLine("The coordinate point" + "(" + (one) + "," + (two) + ")" + "lies in the Three quadrant.");
+                Console.ReadKey();
+            }
+            else if(one>0 && two<0) 
+            {
+                Console.WriteLine("The coordinate point" + "(" + (one) + "," + (two) + ")" + "lies in the Four quadrant.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
