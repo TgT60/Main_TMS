4d1a3051b74e6f96039bf90ec918709a544e912e
TgT60 <dima.icigo@yandex.ru>
2022-09-06 13:26:58 +0300

    Paragraph 3
    -Ex5
    -Completed

---
+++ Hw3.4/Ex5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1N");
+            string NumO = Console.ReadLine();
+            int one = Convert.ToInt32(NumO);
+            if (one >=18 )
+            {
+                Console.WriteLine(one+" You can voice ");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine(one + " You can't voice ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
