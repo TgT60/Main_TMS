a766862b901f22bae365d4f657a25627345fafee
TgT60 <dima.icigo@yandex.ru>
2022-09-12 16:49:57 +0300

    Paragraph 3
    ex 4

---
+++ Paragraph 3/Ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Imput radius");
+            double number = Convert.ToDouble(Console.ReadLine());
+            double perimeter = number * 2 * 3.14;
+            double area = 3.14 * (number *number);
+            Console.WriteLine($"AREA = {area}");
+            Console.WriteLine($"Perimeter = {perimeter}");
+            Console.ReadLine();
+        }
+    }
+}
