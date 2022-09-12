edd1be9c31698fbeeb1cc0070b5525476cf462a6
TgT60 <dima.icigo@yandex.ru>
2022-09-12 16:28:33 +0300

    Paragraph 3
    Ex3

---
+++ Paragraph 3/Ex3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string pasword = Console.ReadLine();
+            for (int i = 1; i < 3; i++)
+            {
+                if (name == "name" && pasword == "pasword")
+                {
+                    i = 3;
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("try again");
+                    name = Console.ReadLine();
+                    pasword = Console.ReadLine();
+
+                }
+
+            }
+
+        }
+    }
+}
