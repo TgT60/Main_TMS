795e513380833d7559812876129173a45ff930cc
TgT60 <dima.icigo@yandex.ru>
2022-09-12 15:18:20 +0300

    Paragraph 3
    Ex2

---
+++ Paragraph 3/Ex2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            string c = Console.ReadLine();
+            Console.WriteLine("Enter Hight");
+            int hight = Convert.ToInt32 (Console.ReadLine());
+            for (int b = 0; b < hight; b++)
+            {
+                for (int i = hight; i > b; i--)
+                {
+                    Console.Write(c);
+                }
+                Console.WriteLine();
+            }
+        
+            Console.ReadKey();
+        }
+    }
+}
