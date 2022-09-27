9fa2174aababb6c1d9e4993780bc177097ce8d0b
TgT60 <dima.icigo@yandex.ru>
2022-09-27 21:36:27 +0300

    ff

---
+++ Shape/Shape/Text.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shape
+{
+    class Text : IPrintable
+    {
+        public void EnterText()
+        {
+            ChoseLocation choseLocation = new ChoseLocation();
+            choseLocation.EnterX_Y();
+            choseLocation.Ox();
+            choseLocation.Oy();
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+        }
+    }
+}
