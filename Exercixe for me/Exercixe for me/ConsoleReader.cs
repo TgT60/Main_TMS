a51cecd1fca2ccae84699559f1aee8e5afcdec5e
TgT60 <dima.icigo@yandex.ru>
2022-10-10 15:40:40 +0300

    Work or not?

---
+++ Exercixe for me/Exercixe for me/ConsoleReader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercixe_for_me
+{
+   
+    class ConsoleReader
+    {
+        public delegate ConsoleKey MethodContainer();
+        public delegate void Exit();
+        public event MethodContainer KeyPressed;
+        public event Exit ExitKeyPressed;
+
+        public void logic()
+        {                
+            KeyHandler console2 = new KeyHandler();
+            
+            KeyPressed += console2.Enter;
+            ExitKeyPressed += console2.Exit;
+
+            while (true)
+            {
+                if (KeyPressed() == ConsoleKey.X)
+                {
+                    Console.WriteLine("   Work");
+                    ExitKeyPressed();
+                }
+                else
+                {
+                    Console.WriteLine("   Don't work");         
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Exercixe for me/Exercixe for me/KeyHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercixe_for_me
+{
+    
+    class KeyHandler
+    {    
+        public ConsoleKey Enter()
+        {          
+            ConsoleKey a = Console.ReadKey().Key;
+            return a;
+        }
+        public void  Exit()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
