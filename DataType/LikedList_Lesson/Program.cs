98a0e300631924ada21c47e7fe9a1f502c627a80
TgT60 <dima.icigo@yandex.ru>
2022-10-13 00:59:41 +0300

    First

---
+++ DataType/LikedList_Lesson/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LikedList_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Table<int, string, bool> table = new Table<int,string, bool>();
+            table.AddRow(2, "d", true);
+            table.AddRow(3, "hf", false);
+            table.AddRow(4, "gf", false);
+            table.AddRow(5, "g", true);
+
+            table.Print();
+        }
+    }
+}
---
+++ DataType/LikedList_Lesson/Table.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LikedList_Lesson
+{
+    public class Table<T, U, Z>
+    {
+        public  static List<T> Ferst { get; set; }
+        public static List<U> Second { get; set; }
+        public static List<Z> Three { get; set; }
+        public Table()
+        {
+            Ferst = new List<T>();
+            Second = new List<U>();
+            Three = new List<Z>();
+        }
+ 
+        public void AddRow(T ferst, U second, Z three)
+        {
+            Ferst.Add(ferst);
+            Second.Add(second);
+            Three.Add(three);
+        }
+        public void Print2()
+        {
+            Ferst.GetType();
+            Second.GetType();
+            Three.GetType();
+        }
+        public void Print()
+        {
+            for (int i = 0; i < Ferst.Count; i++)
+            {
+                Console.WriteLine($"|{Ferst[i]}|{Second[i]}|{Three[i]}|");
+            }                   
+        }
+    }
+}
