de2a619dd30941b431065b59e40aad16f54b1762
TgT60 <dima.icigo@yandex.ru>
2022-11-16 19:07:04 +0300

    Async how i wanted

--- Async_My_LIife/Async_Really_Chached_My_Mind/Program.cs
+++ Async_My_LIife/Async_Really_Chached_My_Mind/Program.cs
@@ -12,19 +12,19 @@ namespace Async_Really_Chached_My_Mind
             асtionsWhitPc.SitOnPc();
 
             var makeApp = new App();
-            Texture texture = await makeApp.WorkWithApp();
+            var Tusk = makeApp.WorkWithApp();
 
             var makePotion = new Pill();
             makePotion.DrinkPill();
 
             var downloadApp = new Download();            
-            Soft soft = await downloadApp.DowloadNewApp();
+            var soft = downloadApp.DowloadNewApp();
 
             var makeNotes = new Notes();
             makeNotes.MakeNotesAboutWork();
 
             var testSoft = new Reupload();
-            testSoft.TestNewFiture(soft, texture);
+            testSoft.TestNewFiture(await soft, await Tusk);
         }
     }
 }
